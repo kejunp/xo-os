32665fc354af3f9b72ccf7fef839f52c1c939712
Kejun Pan <kejunp@users.noreply.github.com>
2025-10-03 16:25:12 -0400

    feat: create a helpers/defs.h with some typedefs and macros designed to mirror stdint, stddef, etc.

---
+++ helpers/defs.h
@@ -0,0 +1,27 @@
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef unsigned long  uint64_t;
+
+typedef char  int8_t;
+typedef short int16_t;
+typedef int   int32_t;
+typedef long  int64_t;
+
+typedef unsigned long uintptr_t;
+
+#ifndef _SIZE_T_DEFINED
+#define _SIZE_T_DEFINED
+typedef long long unsigned int size_t;
+#endif
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+typedef uint64_t pde_t;
+
