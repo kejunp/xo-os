9097ff14558bf2685c286a74ffd51b57dac580bf
Kejun Pan <kejunp@users.noreply.github.com>
2025-10-07 21:09:22 -0400

    change uint64_t from unsigned long to unsigned long long to silence warnings

--- helpers/defs.h
+++ helpers/defs.h
@@ -2,10 +2,10 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-typedef unsigned long  uint64_t;
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
 
 typedef char  int8_t;
 typedef short int16_t;
